020c7d8cdc14e7be54a8197cac1a474cfb73efc5
Gabriel Avila <gabriel.bravin99@gmail.com>
2025-02-28 22:59:46 -0300

    Projetos Alura

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,15 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let numeroMinimo = document.getElementById('de').value;
+    let numeroMaximo = document.getElementById('ate').value;
+    alert(quantidade);
+    alert(numeroMinimo);
+    alert(numeroMaximo);
+}
+
+function gerarNumAleatorio(de, ate) {
+    return Math.random() * (ate - de) + de;
+}
+
+let numAleatorio = gerarNumAleatorio(numeroMinimo, numeroMaximo);
+alert(numAleatorio);

8438f87c2057c73f0a5fe9c654bec57710f4de0c
Gabriel Avila <gabriel.bravin99@gmail.com>
2025-03-01 16:43:49 -0300

    att

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,17 +1,20 @@
 let numero = []
 
-function sortear() {
-    let quantidade = parseInt(document.getElementById('quantidade').value);
-    let de = parseInt(document.getElementById('de').value);
-    let ate = parseInt(document.getElementById('ate').value);
+function sortear() { //funcao para sortear numero
+    let quantidade = parseInt(document.getElementById('quantidade').value); //quantidade de numeros para gerar
+    let de = parseInt(document.getElementById('de').value); //numero inicial
+    let ate = parseInt(document.getElementById('ate').value); //numero final
 
     for (let i = 0; i < quantidade; i++) {
         numero.push(gerandoNumero(de, ate));
     }
-    alert(numero);
+    
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numero}</label>`
 }
 
-function gerandoNumero(min, max) {
+function gerandoNumero(min, max) { //funçao para gerar numero aleatorio inteiro
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 

671bae0ad0bceec53ae2c3a462c4403ef7ad0cac
Gabriel Avila <gabriel.bravin99@gmail.com>
2025-03-15 23:45:29 -0300

    implementa funções para compra de ingressos com validação de quantidade

---
+++ ingresso/js/app.js
@@ -0,0 +1,36 @@
+
+
+
+function comprar() {
+    let tipo = document.getElementById("tipo-ingresso");
+    let qtd = parseInt(document.getElementById("qtd").value);
+
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    }else if (tipo.value == 'Cadeira superior'){
+        comprarSuperior(qtd);
+    }
+
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para tipo cadeira superior');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
